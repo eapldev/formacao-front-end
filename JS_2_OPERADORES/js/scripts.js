bfea701ab92637900f4baf5d10cfa26b1fb9be0d
eapldev <eduardoaparecido.dev@gmail.com>
2024-04-13 09:47:05 -0300

    Conclusão Tipos de dados e operadores em JavaScript

--- JS_2_OPERADORES/js/scripts.js
+++ JS_2_OPERADORES/js/scripts.js
@@ -1,68 +1,103 @@
-// 1 - Number
-console.log(typeof 2)
-console.log(typeof 5.14)
-console.log(typeof -127)
+// // 1 - Number
+// console.log(typeof 2)
+// console.log(typeof 5.14)
+// console.log(typeof -127)
 
-// 2 - Operações . Aritméticas
-console.log(2 + 4)
-console.log(10 - 5)
-console.log(10 * 15)
-console.log(20 / 2)
-console.log(10 + (15 * 2))
+// // 2 - Operações . Aritméticas
+// console.log(2 + 4)
+// console.log(10 - 5)
+// console.log(10 * 15)
+// console.log(20 / 2)
+// console.log(10 + (15 * 2))
 
-// 3 - Special Numbers
-console.log(typeof Infinity)
+// // 3 - Special Numbers
+// console.log(typeof Infinity)
 
-console.log(typeof -Infinity)
+// console.log(typeof -Infinity)
 
-console.log(12 * "asd")
+// console.log(12 * "asd")
 
-console.log(typeof NaN)
+// console.log(typeof NaN)
 
-// 4 - Strings
-console.log(typeof"Teste")
-console.log(typeof'Teste 2')
-console.log(`25`)
+// // 4 - Strings
+// console.log(typeof"Teste")
+// console.log(typeof'Teste 2')
+// console.log(`25`)
 
-// 5 - Símbolos especial em string
-console.log("Testando a \n quebra de linha")
-console.log("Espaçamento \t de tab")
+// // 5 - Símbolos especial em string
+// console.log("Testando a \n quebra de linha")
+// console.log("Espaçamento \t de tab")
 
-// 6 - Concatenação
-console.log("Oi," + " tudo" + " bem?")
+// // 6 - Concatenação
+// console.log("Oi," + " tudo" + " bem?")
 
-console.log(`Testando ` + `com ` + `crase!`)
+// console.log(`Testando ` + `com ` + `crase!`)
 
-// 7 - Templates Strings
-console.log(`A soma de 2 + 2 é: ${2 + 2}`)
+// // 7 - Templates Strings
+// console.log(`A soma de 2 + 2 é: ${2 + 2}`)
 
-console.log(`Podemos executar qualquer coisa aqui ${console.log("Testing")}`)
+// console.log(`Podemos executar qualquer coisa aqui ${console.log("Testing")}`)
 
-// 8 - Boolean
-console.log(true)
+// // 8 - Boolean
+// console.log(true)
 
-console.log(5 > 20)
-console.log(30 > 10)
-console.log(typeof false)
+// console.log(5 > 20)
+// console.log(30 > 10)
+// console.log(typeof false)
 
-// 9 - Comparações
+// // 9 - Comparações
 
-// Maior e menor: > e <
-// Maior ou igual e menor ou igual: >= e <=
-// Igual: ==
-// Diferente: !=
-// Idêntico: ===
-// Recebe: =
+// // Maior e menor: > e <
+// // Maior ou igual e menor ou igual: >= e <=
+// // Igual: ==
+// // Diferente: !=
+// // Idêntico: ===
+// // Recebe: =
 
-console.log(8 <= 8)
-console.log(8 < 8)
-console.log(10 == 10)
-console.log(10 >= 10)
-console.log(10 < 10)
-console.log(10 != 5)
+// console.log(8 <= 8)
+// console.log(8 < 8)
+// console.log(10 == 10)
+// console.log(10 >= 10)
+// console.log(10 < 10)
+// console.log(10 != 5)
 
-// 10 - Idêntico
-console.log(10 == "10")
-console.log(10 === "10")
-console.log(8 != "8")
-console.log(8 !== "8")
+// // 10 - Idêntico
+// console.log(10 == "10")
+// console.log(10 === "10")
+// console.log(8 != "8")
+// console.log(8 !== "8")
+
+// 11 - Operadores Lógicos
+console.log(true && true)
+
+console.log(true && false)
+
+console.log(5 > 2 && 10 < 20)
+
+console.log(10 > 5 && "Eduardo" === 2)
+
+console.log(10 > 5 || "Eduardo" === 2)
+
+console.log(5 < 2 || 9 > 200)
+
+console.log(!true)
+
+console.log(!5 > 4)
+
+// 12 - Empty Values
+console.log(typeof null, typeof undefined)
+
+console.log(null === undefined)
+
+console.log(null == undefined)
+
+console.log(null == false)
+
+console.log(undefined == false)
+
+// 13 - Mudanças de tipos
+console.log(10 * null)
+console.log("teste" * "opa")
+console.log(10 + 1)
+console.log("10" + 1)
+console.log("10" - 1) 

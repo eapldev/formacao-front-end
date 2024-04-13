2433299400426f8ea5ffbe0ae2189083fd10c8e0
eapldev <eduardoaparecido.dev@gmail.com>
2024-04-12 21:07:26 -0300

    Entendendo tipos de dados (Number, Aritméticos, Special Numbers, String, Concatenação, Interpolação, Booleanos etc...)

---
+++ JS_2_OPERADORES/js/scripts.js
@@ -0,0 +1,68 @@
+// 1 - Number
+console.log(typeof 2)
+console.log(typeof 5.14)
+console.log(typeof -127)
+
+// 2 - Operações . Aritméticas
+console.log(2 + 4)
+console.log(10 - 5)
+console.log(10 * 15)
+console.log(20 / 2)
+console.log(10 + (15 * 2))
+
+// 3 - Special Numbers
+console.log(typeof Infinity)
+
+console.log(typeof -Infinity)
+
+console.log(12 * "asd")
+
+console.log(typeof NaN)
+
+// 4 - Strings
+console.log(typeof"Teste")
+console.log(typeof'Teste 2')
+console.log(`25`)
+
+// 5 - Símbolos especial em string
+console.log("Testando a \n quebra de linha")
+console.log("Espaçamento \t de tab")
+
+// 6 - Concatenação
+console.log("Oi," + " tudo" + " bem?")
+
+console.log(`Testando ` + `com ` + `crase!`)
+
+// 7 - Templates Strings
+console.log(`A soma de 2 + 2 é: ${2 + 2}`)
+
+console.log(`Podemos executar qualquer coisa aqui ${console.log("Testing")}`)
+
+// 8 - Boolean
+console.log(true)
+
+console.log(5 > 20)
+console.log(30 > 10)
+console.log(typeof false)
+
+// 9 - Comparações
+
+// Maior e menor: > e <
+// Maior ou igual e menor ou igual: >= e <=
+// Igual: ==
+// Diferente: !=
+// Idêntico: ===
+// Recebe: =
+
+console.log(8 <= 8)
+console.log(8 < 8)
+console.log(10 == 10)
+console.log(10 >= 10)
+console.log(10 < 10)
+console.log(10 != 5)
+
+// 10 - Idêntico
+console.log(10 == "10")
+console.log(10 === "10")
+console.log(8 != "8")
+console.log(8 !== "8")

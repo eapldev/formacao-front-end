613548fe638dd881b6bde24c881fa17bf7a1d59d
eapldev <eduardoaparecido.dev@gmail.com>
2024-05-11 21:44:39 -0300

    Conceitos de Eventos no JavaScript 2/2

--- JS_10_EVENTOS/js/scripts.js
+++ JS_10_EVENTOS/js/scripts.js
@@ -70,5 +70,61 @@ mouseEvents.addEventListener("mouseup", () => {
 });
 
 mouseEvents.addEventListener("dblclick", () => {
-  console.log("Click duplo")
-})
+  console.log("Clique Duplo");
+});
+
+// 8 - movimento do mouse
+document.addEventListener("mousemove", (e) => {
+  // console.log(`No eixo X: ${e.x}`);
+  // console.log(`No eixo Y: ${e.y}`);
+});
+
+// 9 - evento de scroll
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY > 200) {
+    console.log("Passamos de 200px");
+  }
+});
+
+// 10 - evento de focus/blur
+const input = document.querySelector("#my-input");
+
+input.addEventListener("focus", () => {
+  console.log("Entrou no input");
+});
+
+input.addEventListener("blur", () => {
+  console.log("Saiu do input");
+});
+
+// 11 - evento carregamento
+window.addEventListener("load", () => {
+  console.log("A página carregou!");
+});
+
+// window.addEventListener("beforeunload", (e) => {
+//   e.preventDefault();
+//   e.returnValue = "";
+// });
+
+// 12 - debounce
+const debounce = (f, delay) => {
+  let timeout;
+
+  return (...arguments) => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(() => {
+      f.apply(...arguments);
+    }, delay);
+  };
+};
+
+window.addEventListener(
+  "mousemove",
+  debounce(() => {
+    console.log("Executando a cada 400ms");
+  }, 400)
+);
